f8921c81c2272a4f10dc094a582ee7e5480ed5f6
Fardin Ahsan <fardin2002@cseku.ac.bd>
2023-03-09 02:47:47 +0600

    Issues:
     Only one trim is being possible. No way to do multiple trims.
     Audio in target file is not synchronized.
     Audio and Video attributes is not being copied from the original
    file.

--- VideoEditor/src/TrimFiles/singleTrimming.java
+++ VideoEditor/src/TrimFiles/singleTrimming.java
@@ -12,16 +12,18 @@ public class singleTrimming {
             if(!target.exists()) target.createNewFile();
             
             VideoAttributes video = new VideoAttributes();
-            video.setCodec(VideoAttributes.DIRECT_STREAM_COPY);
-//            video.setBitRate(new Integer(160000));
-//            video.setFrameRate(new Integer(15));
+//            video.setCodec(VideoAttributes.DIRECT_STREAM_COPY);
+            video.setCodec("flv");
+            video.setBitRate(new Integer(160000));
+            video.setFrameRate(new Integer(15));
 //            video.setSize(new VideoSize(400, 300));
             
             AudioAttributes audio = new AudioAttributes();
-            audio.setCodec(AudioAttributes.DIRECT_STREAM_COPY);
-//            audio.setBitRate(new Integer(64000));
-//            audio.setChannels(new Integer(1));
-//            audio.setSamplingRate(new Integer(22050));
+//            audio.setCodec(AudioAttributes.DIRECT_STREAM_COPY);
+            audio.setCodec("libmp3lame");
+            audio.setBitRate(new Integer(64000));
+            audio.setChannels(new Integer(1));
+            audio.setSamplingRate(new Integer(22050));
             
             EncodingAttributes attrs = new EncodingAttributes();
             attrs.setFormat("flv");
